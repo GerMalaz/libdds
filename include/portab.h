bacf95d36152f1b1021f422f5c8f3c897a670338
Gerardo Esteban Malazdrewicz <gerardo@malazdrewicz.com.ar>
2023-02-06 06:17:09 -0400

    Option to disable static initialization

--- include/portab.h
+++ include/portab.h
@@ -25,7 +25,7 @@
 
 #elif defined (__linux) || defined (__APPLE__)
   #include <unistd.h>
-  #if not defined(DDS_NO_STATIC_INIT)
+  #if !defined(DDS_NO_STATIC_INIT)
   /* DLL uses a constructor function for initialization */
   #endif
 
